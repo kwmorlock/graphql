65cff38065660ff69c986d7529c12905a3c9029b
kwmorlock <kmorlockcodes@gmail.com>
2020-12-09 13:18:53 -0500

    set: server.js express

--- server.js
+++ server.js
@@ -21,7 +21,18 @@ var schema = buildSchema(`
 
 // root resolver
 var root = {
-    message: () => 'Meow!'
+  message: () => "Meow!",
 };
 
+// Create an express server and a GraphQL endpoint
+var app = express();
+app.use(
+  "/graphql",
+  express_graphql({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+); //endpoint
 
+app.listem(4000, () => console.log("Server now running!"));
