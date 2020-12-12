f7a70ad51c0464333dffc7b052432a3c98a9525f
kwmorlock <kmorlockcodes@gmail.com>
2020-12-12 09:19:33 -0500

    set: server2.js getCourses function

--- server2.js
+++ server2.js
@@ -28,38 +28,50 @@ var schema = buildSchema(`
 `);
 
 var coursesData = [
-    {
-        id: 1,
-        title: 'The Complete Node.js Developer Course',
-        author: 'Andrew Mead, Rob Percival',
-        description: 'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
-        topic: 'Node.js',
-        url: 'https://codingthesmartway.com/courses/nodejs/'
-    },
-    {
-        id: 2,
-        title: 'Node.js, Express & MongoDB Dev to Deployment',
-        author: 'Brad Traversy',
-        description: 'Learn by example building & deploying real-world Node.js applications from absolute scratch',
-        topic: 'Node.js',
-        url: 'https://codingthesmartway.com/courses/nodejs-express-mongodb/'
-    },
-    {
-        id: 3,
-        title: 'JavaScript: Understanding The Weird Parts',
-        author: 'Anthony Alicea',
-        description: 'An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
-        topic: 'JavaScript',
-        url: 'https://codingthesmartway.com/courses/understand-javascript/'
-    }
-]
+  {
+    id: 1,
+    title: "The Complete Node.js Developer Course",
+    author: "Andrew Mead, Rob Percival",
+    description:
+      "Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!",
+    topic: "Node.js",
+    url: "https://codingthesmartway.com/courses/nodejs/",
+  },
+  {
+    id: 2,
+    title: "Node.js, Express & MongoDB Dev to Deployment",
+    author: "Brad Traversy",
+    description:
+      "Learn by example building & deploying real-world Node.js applications from absolute scratch",
+    topic: "Node.js",
+    url: "https://codingthesmartway.com/courses/nodejs-express-mongodb/",
+  },
+  {
+    id: 3,
+    title: "JavaScript: Understanding The Weird Parts",
+    author: "Anthony Alicea",
+    description:
+      "An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.",
+    topic: "JavaScript",
+    url: "https://codingthesmartway.com/courses/understand-javascript/",
+  },
+];
 
-var gerCourse = function(args) {
-    return coursesData.filter(course =>{
-        return course.id == id;
-    })[0];
-}
+var getCourse = function (args) {
+  var id = args.id;
+  return coursesData.filter((course) => {
+    return course.id == id;
+  })[0];
+};
 
+var getCourses = function (args) {
+  if (args.topic) {
+    var topic = args.topic;
+    return coursesData.filter((courses) => course.topic === topic);
+  } else {
+    return coursesData;
+  }
+};
 
 // resolver, attach a function that is called each time
 // a query from our schema needs to be executed, because
@@ -69,7 +81,7 @@ var gerCourse = function(args) {
 //getCourse and getCourses are the functions
 var root = {
   course: getCourse,
-  courses: getCourses
+  courses: getCourses,
 };
 
 // Create an express server and a GraphQL endpoint
